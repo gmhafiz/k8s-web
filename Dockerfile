345f01bf240015ab62d46085cd600ec25c4c9287
Hafiz Shafruddin <gmhafiz@gmail.com>
2023-06-17 20:26:04 +1000

    initial

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# docker build -t gmhafiz/web .
+FROM node:18 as build-stage
+
+RUN npm install -g http-server
+
+WORKDIR /app
+
+COPY package*.json ./
+RUN npm install
+
+COPY . .
+RUN npm run build
+
+FROM nginx:stable as production-stage
+
+COPY --from=build-stage /app/dist /usr/share/nginx/html
+
+EXPOSE 80
+
+CMD ["nginx", "-g", "daemon off;"]
---
+++ src/views/RandomsView.vue
@@ -0,0 +1,68 @@
+<script  lang="ts">
+
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  mounted() {
+    this.get()
+  },
+  data() {
+    return {
+      randoms: [],
+
+      address:'http://localhost:3081'
+    }
+  },
+  methods: {
+    async get() {
+
+      this.randoms = await fetch(`${this.address}/randoms`)
+          .then((res => res.json()))
+          .catch((err) => {
+            console.error(err)
+          });
+    },
+    add() {
+      fetch(`${this.address}/randoms`, {
+        method: "POST"
+      })
+        .then(() => {
+          this.get()
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="randoms">
+
+    <div style="margin-top: 1rem; margin-bottom: 1rem;">
+      <button
+          style="padding: 0.5rem; background-color: rgba(72,154,238,0.79); color: rgb(255,255,255,0.86)"
+          @click="add"
+      >
+        Add New UUID
+      </button>
+    </div>
+
+    Last 10 Random UUID
+
+    <div v-for="item in randoms" :key="item.id" style="display: flex; font-family: monospace ">
+      <div style="min-width: 1.5rem;">
+        {{ item.id }}
+      </div>
+      <div>
+        : {{ item.name }}
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
